6cc52a60f297990a42cfefccb7e9a87f4dd1cfc9
Christ_kamina <christngoloknc@gmail.com>
2022-01-15 22:26:56 +0100

    changes

--- app/src/main/java/com/example/kitumaini_app/Next_Activty.java
+++ app/src/main/java/com/example/kitumaini_app/Next_Activty.java
@@ -2,7 +2,13 @@ package com.example.kitumaini_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.time.Instant;
 
 public class Next_Activty extends AppCompatActivity {
 
@@ -10,5 +16,22 @@ public class Next_Activty extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next__activty);
+        Button button = findViewById(R.id.beneficiarebutton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              Intent intent = new Intent(Next_Activty.this, Login_beneficiare_activity.class);
+              startActivity(intent);
+            }
+        });
+
+        Button b = findViewById(R.id.prestatairebutton);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Next_Activty.this, Login_prestataire_Activity.class);
+                startActivity(i);
+            }
+        });
     }
 }
