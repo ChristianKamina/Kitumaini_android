f2f765633e2d4db7698ef189c0142f041a2150ab
Christ_kamina <christngoloknc@gmail.com>
2022-01-15 01:38:06 +0100

    Splash screen ok

--- app/src/main/java/com/example/kitumaini_app/MainActivity.java
+++ app/src/main/java/com/example/kitumaini_app/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.kitumaini_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+
+    Handler h = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, Next_Activty.class);
+                startActivity(i);
+                finish();
+            }
+        }, 5000);
     }
 }
