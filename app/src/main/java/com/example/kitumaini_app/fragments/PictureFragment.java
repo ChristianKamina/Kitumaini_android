981c6bb382f0f75d51cf29a3557de6668e6d40d3
Ngams9 <ngamstamfutu@gmail.com>
2022-01-16 22:48:29 +0100

    Sixieme commit: picture

--- app/src/main/java/com/example/kitumaini_app/fragments/PictureFragment.java
+++ app/src/main/java/com/example/kitumaini_app/fragments/PictureFragment.java
@@ -1,18 +1,43 @@
 package com.example.kitumaini_app.fragments;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.Manifest;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.kitumaini_app.R;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 
 public class PictureFragment extends Fragment {
 
+    ImageView img_picture;
+
+    private static final int IMAGE_PICK_GALLERY_CODE = 1000;
+    private static final int IMAGE_PICK_CAMERA_CODE = 1001;
+    private static final int MY_CAMERA_PERMISSION_CODE = 100;
+    private static final int STORAGE_REQUEST_CODE = 400;
+    private Uri uri;
+    public String cameraPermission[];
+    public String storagePermission[];
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -20,6 +45,133 @@ public class PictureFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_picture, container, false);
 
+        /*-camera permission-*/
+        cameraPermission = new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        /*-storage permission-*/
+        storagePermission = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+        img_picture = view.findViewById(R.id.img_picture);
+        img_picture.setOnClickListener(v -> {
+
+            if (getActivity().checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+            {
+                requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_PERMISSION_CODE);
+            }
+            else
+            {
+                if (!checkCameraPermission()){
+                    //if permission not allowed
+                    requestCameraPermission();
+                }else {
+
+                    openCamera();
+                }
+            }
+        });
+
+        view.findViewById(R.id.btn_save_picture).setOnClickListener(v -> {
+            Toast.makeText(getContext(), "Save...", Toast.LENGTH_SHORT).show();
+        });
+
         return view;
+    }
+
+    private void openCamera() {
+        //CropImage.activity().start(NewsPostActivity.this);
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE,"NewPic");
+        values.put(MediaStore.Images.Media.DESCRIPTION,"Image to Text");
+
+        uri = getContext().getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+
+        Intent intentCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intentCamera.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+        startActivityForResult(intentCamera, IMAGE_PICK_CAMERA_CODE);
+    }
+
+    private void requestStoragePermission() {
+
+        ActivityCompat.requestPermissions(getActivity(), storagePermission, STORAGE_REQUEST_CODE);
+    }
+
+    private boolean checkStoragePermission() {
+
+        boolean result = ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                == (PackageManager.PERMISSION_GRANTED);
+
+        return result;
+    }
+
+    private void requestCameraPermission() {
+        ActivityCompat.requestPermissions(getActivity(), cameraPermission, MY_CAMERA_PERMISSION_CODE);
+    }
+
+    private boolean checkCameraPermission() {
+
+        boolean result = ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA)
+                == (PackageManager.PERMISSION_GRANTED);
+        boolean result1 = ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                == (PackageManager.PERMISSION_GRANTED);
+
+        return result && result1;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+
+            case MY_CAMERA_PERMISSION_CODE:
+
+                if (grantResults.length > 0) {
+
+                    boolean cameraAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    boolean writeStorageAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+
+                    if (cameraAccepted && writeStorageAccepted) {
+
+                        openCamera();
+
+                    } else {
+
+                        Toast.makeText(getContext(), "Permission denied!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK){
+
+            if (requestCode == IMAGE_PICK_GALLERY_CODE){
+
+                /*CropImage.activity()
+                        .start(getContext(), this);*/
+
+                CropImage.activity(data.getData())
+                        .setGuidelines(CropImageView.Guidelines.ON)
+                        .start(getContext(), this);
+            }
+
+            if (requestCode == IMAGE_PICK_CAMERA_CODE){
+
+                CropImage.activity(uri)
+                        .setGuidelines(CropImageView.Guidelines.ON)
+                        .start(getContext(), this);
+            }
+        }
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+
+            if (resultCode == RESULT_OK) {
+
+                Uri resultUri = result.getUri();
+                img_picture.setImageURI(resultUri);
+            }
+        }
     }
 }
